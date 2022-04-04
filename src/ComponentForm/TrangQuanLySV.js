b67c2c12f27e60c5afc3d600ffccea78b35d5cf4
Nam <phamhainamkt@gmail.com>
2022-04-04 21:34:07 +0700

    thêm link deloy

--- src/ComponentForm/TrangQuanLySV.js
+++ src/ComponentForm/TrangQuanLySV.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import FormDangKy from './FormDangKy'
 import TableDSVS from './TableDSVS'
 
+// Link Deloy: https://bcdn-05-baitap-form-redux-pham-hai-nam-hainamkt.vercel.app/
+
 export default class TrangQuanLySV extends Component {
   render() {
     return (
